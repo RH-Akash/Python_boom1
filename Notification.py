29111499526a12372e688e00504ca717dc76642f
Rimon Hossain Akash <72348124+RH-Akash@users.noreply.github.com>
2020-12-22 13:38:44 +0600

    Add files via upload

---
+++ Notification.py
@@ -0,0 +1,8 @@
+import time
+from plyer import notification
+if __name__== "__main__":
+    notification.notify(
+        title="Please drink water now.",
+        message=" Drinking Water Helps Maintain the Balance of Body Fluids."
+                " Your body is composed of about 60% water. The functions of these bodily fluids",
+    )
